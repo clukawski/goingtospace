308127e10acb734661ea18c6e18f3a6c7a5dacb1
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2014-08-22 13:42:40 -0400

    Commented out unused function.

--- sensors/mpl3115a2/mpl3115a2.go
+++ sensors/mpl3115a2/mpl3115a2.go
@@ -83,52 +83,52 @@ func (d *MPL3115A2) MeasureAltAndPress() (int, int, error) {
 }
 
 
-// NOTE: to remove...
-func (d *MPL3115A2) MeasureHumidAndTemp() (float64, int, error) {
-	data := make([]byte, 4)
-	if err := d.Bus.ReadFromReg(address, fakereg, data); err != nil {
-		return 0, 0, err
-	}
-
-	// Reading 4 bytes of data. First two are status bits (2) humidity data (6, 8), second two are temperature data (8, 6, with the last two bits DNC)
-	// Doing bit shifts here
-
-	// status := uint8(data[0] >> 6) don't need this yet
-	hdata := uint16(data[0] & 0x3f) << 8 | uint16(data[1])
-	tdata := (uint16(data[2]) << 8 | uint16(data[3])) >> 2
-
-
-	// conversion from uint16 to float64
-	var humid float64
-	var temp float64
-
-	hbytes := make([]byte, 2)
-	binary.LittleEndian.PutUint16(hbytes, uint16(hdata))
-
-	tbytes := make([]byte, 2)
-	binary.LittleEndian.PutUint16(tbytes, uint16(tdata))
-
-	hbuf := bytes.NewReader(hbytes)
-	tbuf := bytes.NewReader(tbytes)
-
-	err := binary.Read(hbuf, binary.LittleEndian, &humid)
-	if err != nil {
-		return 0, 0, err
-	}
-	err = binary.Read(tbuf, binary.LittleEndian, &temp)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	// Turn readings into usable data
-	var h float64
-	var t float64
-
-	h = humid / (math.Pow(2, 14) - 1) * 100
-	t = temp / (math.Pow(2, 14) - 1) * 165 - 40
-
-	return h, int(t), nil //cast t as int, as we don't really need decimal places for Celcius temperature readings.
-}
+// NOTE: Remove this after the above function is written
+// func (d *MPL3115A2) MeasureHumidAndTemp() (float64, int, error) {
+// 	data := make([]byte, 4)
+// 	if err := d.Bus.ReadFromReg(address, fakereg, data); err != nil {
+// 		return 0, 0, err
+// 	}
+//
+// 	// Reading 4 bytes of data. First two are status bits (2) humidity data (6, 8), second two are temperature data (8, 6, with the last two bits DNC)
+// 	// Doing bit shifts here
+//
+// 	// status := uint8(data[0] >> 6) don't need this yet
+// 	hdata := uint16(data[0] & 0x3f) << 8 | uint16(data[1])
+// 	tdata := (uint16(data[2]) << 8 | uint16(data[3])) >> 2
+//
+//
+// 	// conversion from uint16 to float64
+// 	var humid float64
+// 	var temp float64
+//
+// 	hbytes := make([]byte, 2)
+// 	binary.LittleEndian.PutUint16(hbytes, uint16(hdata))
+//
+// 	tbytes := make([]byte, 2)
+// 	binary.LittleEndian.PutUint16(tbytes, uint16(tdata))
+//
+// 	hbuf := bytes.NewReader(hbytes)
+// 	tbuf := bytes.NewReader(tbytes)
+//
+// 	err := binary.Read(hbuf, binary.LittleEndian, &humid)
+// 	if err != nil {
+// 		return 0, 0, err
+// 	}
+// 	err = binary.Read(tbuf, binary.LittleEndian, &temp)
+// 	if err != nil {
+// 		return 0, 0, err
+// 	}
+//
+// 	// Turn readings into usable data
+// 	var h float64
+// 	var t float64
+//
+// 	h = humid / (math.Pow(2, 14) - 1) * 100
+// 	t = temp / (math.Pow(2, 14) - 1) * 165 - 40
+//
+// 	return h, int(t), nil //cast t as int, as we don't really need decimal places for Celcius temperature readings.
+// }
 
 // Run starts the sensor data acquisition loop.
 func (d *MPL3115A2) Run() {
