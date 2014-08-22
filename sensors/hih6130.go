3ac842059c8726ac9573762e081a1a6dd748a42c
Conrad Lukawski <conradlukawski@gmail.com>
2014-08-21 20:17:17 -0400

    Fixed some bugs, Init() and Stop() were all wrong. Fixed that.

--- sensors/hih6130.go
+++ sensors/hih6130.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 	"time"
 	"encoding/binary"
+	"bytes"
 	
 	"github.com/kidoman/embd"
 )
@@ -32,14 +33,38 @@ type HIH6130 struct {
 
 // Init sends the high bit to the sensor and "turns it on"
 func (d *HIH6130) Init() error {
-	err := d.Bus.WriteByte(address, embd.High)
-	return err
+	if err := embd.InitGPIO(); err != nil {
+		return err
+	}
+	defer embd.CloseGPIO()
+	
+	if err := embd.SetDirection(1, embd.Out); err != nil {
+		return err
+	}
+
+	if err := embd.DigitalWrite(1, embd.High); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Stop sends the low bit to the sensor and turns it off.
 func (d *HIH6130) Stop() error {
-	err := d.Bus.WriteByte(address, embd.Low)
-	return err
+	if err := embd.InitGPIO(); err != nil {
+		return err
+	}
+	defer embd.CloseGPIO()
+	
+	if err := embd.SetDirection(1, embd.Out); err != nil {
+		return err
+	}
+
+	if err := embd.DigitalWrite(1, embd.Low); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // New returns a handle to a HIH6130 sensor.
@@ -47,7 +72,7 @@ func New(bus embd.I2CBus) *HIH6130 {
 	return &HIH6130{Bus: bus, Poll: pollDelay}
 }
 
-func (d *HIH6130) MeasureHumidAndTemp() (int, int) {
+func (d *HIH6130) MeasureHumidAndTemp() (float64, int, error) {
 	data := make([]byte, 4)
 	if err := d.Bus.ReadFromReg(address, fakereg, data); err != nil {
 		return 0, 0, err
@@ -59,28 +84,34 @@ func (d *HIH6130) MeasureHumidAndTemp() (int, int) {
 	hdata := uint16(data[0] & 0x3f) << 8 | uint16(data[1])
 	tdata := (uint16(data[2]) << 8 | uint16(data[3])) >> 2
 
-	var humid int
-	var temp int
+	var humid float64
+	var temp float64
 
-	hbuf := bytes.NewReader([]byte(hdata))
-	tbuf := bytes.NewReader([]byte(tdata))
+	hbytes := make([]byte, 2)
+	binary.LittleEndian.PutUint16(hbytes, uint16(hdata))
+
+	tbytes := make([]byte, 2)
+	binary.LittleEndian.PutUint16(tbytes, uint16(tdata))
+
+	hbuf := bytes.NewReader(hbytes)
+	tbuf := bytes.NewReader(tbytes)
 
 	err := binary.Read(hbuf, binary.LittleEndian, &humid)
 	if err != nil {
 		return 0, 0, err
 	}
-	err := binary.Read(tbuf, binary.LittleEndian, &temp)
+	err = binary.Read(tbuf, binary.LittleEndian, &temp)
 	if err != nil {
 		return 0, 0, err
 	}
 
 	var h float64
-	var t int
+	var t float64
 	
 	h = humid / (math.Pow(2, 14) - 1) * 100
 	t = temp / (math.Pow(2, 14) - 1) * 165 - 40
 
-	return h, t, nil
+	return h, int(t), nil
 }
 
 // Run starts the sensor data acquisition loop.
@@ -89,7 +120,7 @@ func (d *HIH6130) Run() {
 		d.quit = make(chan struct{})
 		timer := time.Tick(time.Duration(d.Poll) * time.Millisecond)
 
-		var humid int
+		var humid float64
 		var temp int
 
 		for {
@@ -101,14 +132,14 @@ func (d *HIH6130) Run() {
 					temp = t
 				}
 				if err == nil && d.humids == nil && d.temps == nil {
-					d.humid = make(chan float64)
-					d.temp = make(chan int)
+					d.humids = make(chan float64)
+					d.temps = make(chan int)
 				}
 			case d.temps <- temp:
-			case d.humid <- humidity:
+			case d.humids <- humid:
 			case <-d.quit:
 				d.temps = nil
-				d.humid = nil
+				d.humids = nil
 				return
 			}
 		}
