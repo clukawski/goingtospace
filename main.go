77acd0fef1bcc1ccd4ad57694cfdfa7187bad5f7
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2015-05-09 20:15:55 -0400

    sends light sensor data to logger

--- main.go
+++ main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"github.com/kidoman/embd"
 	_ "github.com/kidoman/embd/host/all"
 	"github.com/kidoman/embd/sensor/bh1750fvi"
+	"log"
+	"os"
+	"time"
 )
 
 func main() {
+	logfile, err := os.Create("logfile.log")
+	if err != nil {
+		return
+	}
+	defer logfile.Close()
+
+	logger := log.New(logfile, "", log.LstdFlags|log.microseconds)
+
 	bus := embd.NewI2CBus(1)
 
-	light := bh1750fvi.New("High2",bus)
+	light := bh1750fvi.New("High2", bus)
 	light.Run()
 
 	for {
 		current, _ := light.Lighting()
 		time.Sleep(1000 * time.Millisecond)
-		fmt.Println(current)
+		// fmt.Println(current)
+		logger.Print("light:", current)
 	}
 }
