4112b38376cc57460b812fa7c25d1bbcf632ae51
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2015-05-09 21:26:56 -0400

    light sensor logging runs on ticker

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "fmt"
 	"github.com/kidoman/embd"
 	_ "github.com/kidoman/embd/host/all"
 	"github.com/kidoman/embd/sensor/bh1750fvi"
@@ -16,9 +15,7 @@ func main() {
 		return
 	}
 	defer logfile.Close()
-
 	log.Print("Logging to logfile.log")
-
 	logger := log.New(logfile, "", log.LstdFlags|log.Lmicroseconds)
 
 	bus := embd.NewI2CBus(1)
@@ -26,10 +23,10 @@ func main() {
 	light := bh1750fvi.New("High2", bus)
 	light.Run()
 
-	for {
+	ticker := time.Tick(time.Second)
+
+	for range ticker {
 		current, _ := light.Lighting()
-		time.Sleep(1000 * time.Millisecond)
-		// fmt.Println(current)
 		logger.Print("light:", current)
 	}
 }
